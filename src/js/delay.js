ca607938e69da959107635180df994f06af9e33a
Maria <mashero4ka@gmail.com>
2020-11-07 19:27:32 +0200

    first task

--- src/js/delay.js
+++ src/js/delay.js
@@ -1,4 +1,4 @@
-const delay = ms => {
+export const delay = ms => {
     return new Promise(resolve => {
         setTimeout(() => { resolve(ms); }, ms);
     });
@@ -9,4 +9,5 @@ const delay = ms => {
   // Вызовы функции для проверки
   delay(2000).then(logger); // Resolved after 2000ms
   delay(1000).then(logger); // Resolved after 1000ms
-  delay(1500).then(logger); // Resolved after 1500ms
+delay(1500).then(logger); // Resolved after 1500ms
+
